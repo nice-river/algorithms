f2cb7a15f8425d6a73959df4675dc2851a2f028f
niceriver <foresetniceriver@outlook.com>
2023-10-24 16:27:54 +0800

    my leetcode lc2272 correct impl

--- leetcode/src/lc2272.rs
+++ leetcode/src/lc2272.rs
@@ -37,6 +37,20 @@ mod tests {
         assert_eq!(Solution::largest_variance(s), ans);
     }
 
+    #[test]
+    fn test5() {
+        let s = "ababb".to_owned();
+        let ans = 2;
+        assert_eq!(Solution::largest_variance(s), ans);
+    }
+
+    #[test]
+    fn test6() {
+        let s = "aab".to_owned();
+        let ans = 1;
+        assert_eq!(Solution::largest_variance(s), ans);
+    }
+
     fn to_vec2d<T, O, I>(a: O) -> Vec<Vec<T>>
     where
         T: Clone,
@@ -54,59 +68,69 @@ use crate::*;
 
 struct Solution {}
 
-use std::collections::BTreeSet;
-
 const ALPHA: usize = 26;
 
 impl Solution {
     pub fn largest_variance(s: String) -> i32 {
         let s = s.as_bytes();
         let mut ans = 0;
-        let mut sets: Vec<BTreeSet<i32>> = vec![BTreeSet::new(); ALPHA];
+        let mut cnts = vec![0; ALPHA];
+        let mut stks: Vec<Vec<Vec<(i32, usize)>>> = vec![vec![vec![]; ALPHA]; ALPHA];
         for i in 0..ALPHA {
-            sets[i].insert(0);
+            for j in 0..ALPHA {
+                stks[i][j].push((0, 0));
+            }
         }
-        let mut cnts = vec![0i32; ALPHA];
-        for &x in s.iter() {
-            let x = (x - b'a') as usize;
+        let mut pos = vec![0; ALPHA];
+        for i in 0..s.len() {
+            let x = (s[i] - b'a') as usize;
             cnts[x] += 1;
-            let mut it = sets[x].iter();
-            let mut succ = false;
-            let mut p = 0;
-            while let Some(&v) = it.next() {
-                if v == cnts[x] {
+            pos[x] = i + 1;
+            for y in 0..ALPHA {
+                if y == x {
                     continue;
-                } else {
-                    succ = true;
-                    p += v;
-                    break;
                 }
-            }
-            if succ {
-                for y in b'a'..=b'z' {
-                    let y = (y - b'a') as usize;
-                    if x == y {
-                        continue;
-                    }
-                    let d = cnts[x] - cnts[y];
-                    let mut it = sets[y].iter().rev();
-                    succ = false;
-                    let mut t = p;
-                    while let Some(&v) = it.next() {
-                        if v == cnts[y] {
-                            continue;
-                        } else {
-                            succ = true;
-                            t -= v;
-                            break;
-                        }
-                    }
-                    if succ {
-                        ans = ans.max((d - t).abs());
+                let stk = &stks[x][y];
+                let mut l = 0;
+                let mut r = stk.len();
+                let p = pos[y];
+                let mut q = stk.len();
+                while l < r {
+                    let m = (l + r) / 2;
+                    if stk[m].1 < p {
+                        q = m;
+                        l = m + 1;
+                    } else {
+                        r = m;
                     }
                 }
+                if q != stk.len() {
+                    ans = ans.max(cnts[x] - cnts[y] - stk[q].0);
+                }
+                let stk = &stks[y][x];
+                let mut l = 0;
+                let mut r = stk.len();
+                let p = pos[y];
+                let mut q = stk.len();
+                while l < r {
+                    let m = (l + r) / 2;
+                    if stk[m].1 < p {
+                        q = m;
+                        l = m + 1;
+                    } else {
+                        r = m;
+                    }
+                }
+                if q != stk.len() {
+                    ans = ans.max(cnts[y] - cnts[x] - stk[q].0);
+                }
+                if stks[x][y].last().unwrap().0 > cnts[x] - cnts[y] {
+                    stks[x][y].push((cnts[x] - cnts[y], i + 1));
+                }
+                if stks[y][x].last().unwrap().0 > cnts[y] - cnts[x] {
+                    stks[y][x].push((cnts[y] - cnts[x], i + 1));
+                }
             }
-            sets[x].insert(cnts[x]);
         }
         ans
     }
